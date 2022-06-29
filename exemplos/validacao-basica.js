8b432a82b7940bf1669a84fb5e8d0a9d9ed3aa35
alencartha <alencar_tha@hotmail.com>
2022-06-29 11:48:10 -0300

    refactor: codigo js limpart input

--- exemplos/validacao-basica.js
+++ exemplos/validacao-basica.js
@@ -3,13 +3,15 @@ let input2 = document.getElementById('input2')
 let button = document.getElementById('buttonClick')
 
 button.addEventListener('click', () => {
-    console.log(somarNumeros(input1.value, input2.value))
+    alert(`A soma dos números é: ${somarNumeros(input1.value, input2.value)}`)
+    input1.value = ''
+    input2.value = ''
 })
 
 function somarNumeros(numero1, numero2) {
-    if (typeof numero1 == "number" && typeof numero2 == "number") {
+    if (typeof numero1 === "number" && typeof numero2 === "number") {  
         return numero1 + numero2
-    } else {
+    } else { 
         return Number(numero1) + Number(numero2)
     }
 }
