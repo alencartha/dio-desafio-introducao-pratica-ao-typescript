45b3dc7083a2f5d309e094f702d33d044bd29310
alencartha <alencar_tha@hotmail.com>
2022-06-29 17:09:42 -0300

    tsconfig.json outDir e rootDir

--- app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var button = document.getElementById('buttonClick');
-button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
-    console.log('funcionou');
-});
