64fd6c942317210631c76f78fdae325da577ff3c
alencartha <alencar_tha@hotmail.com>
2022-06-29 16:36:36 -0300

    incluindo callback em funcoes

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 "use strict";
-function somarValores(input1, input2) {
-    if (typeof input1 === 'string' || typeof input2 === 'string') {
-        return input1.toString() + input2.toString();
+function somarValores(number1, number2) {
+    if (typeof number1 === 'string' || typeof number2 === 'string') {
+        return number1.toString() + number2.toString();
     }
     else {
-        return input1 + input2;
+        return number1 + number2;
     }
 }
-function somarValoresNumericos(input1, input2) {
-    return input1 + input2;
-}
-function printaValoresNumericos(input1, input2) {
-    console.log(input1 + input2);
+function printaValoresNumericos(number1, number2) {
+    console.log(number1 + number2);
 }
 console.log(somarValores(1, 5));
 console.log(somarValores('olá', ' tudo bem?'));
-console.log(somarValoresNumericos(1, 5));
+/*function somarValoresNumericos(number1: number, number2: number): number {
+    return number1 + number2
+}*/
+function somarValoresNumericosETratar(number1, number2, callback) {
+    let resultado = number1 + number2;
+    return callback(resultado);
+}
+function aoQuadrado(numero) {
+    return numero * numero;
+}
+function dividirPorEleMesmo(numero) {
+    return numero / numero;
+}
+console.log(somarValoresNumericosETratar(1, 2, aoQuadrado));
+console.log(somarValoresNumericosETratar(1, 2, dividirPorEleMesmo));
--- app.ts
+++ app.ts
@@ -1,20 +1,37 @@
-function somarValores(input1: number | string, input2: number | string) {
-    if (typeof input1 === 'string' || typeof input2 === 'string') {
-        return input1.toString() + input2.toString();
+function somarValores(number1: number | string, number2: number | string) {
+    if (typeof number1 === 'string' || typeof number2 === 'string') {
+        return number1.toString() + number2.toString();
     } else {
-        return input1 + input2
+        return number1 + number2
     }
 
 }
 
-function somarValoresNumericos(input1: number, input2: number): number {
-    return input1 + input2
-}
-
-function printaValoresNumericos(input1: number, input2: number): void {
-    console.log(input1 + input2)
+function printaValoresNumericos(number1: number, number2: number): void {
+    console.log(number1 + number2)
 }
 
 console.log(somarValores(1, 5))
 console.log(somarValores('olá', ' tudo bem?'))
-console.log(somarValoresNumericos(1, 5))
+
+
+
+/*function somarValoresNumericos(number1: number, number2: number): number {
+    return number1 + number2
+}*/
+
+function somarValoresNumericosETratar(number1: number, number2: number, callback:(numero: number)=> number): number {
+    let resultado = number1 + number2;
+    return callback(resultado)
+}
+
+function aoQuadrado(numero: number): number{
+    return numero * numero
+}
+
+function dividirPorEleMesmo(numero: number): number{
+    return numero / numero
+}
+
+console.log(somarValoresNumericosETratar(1, 2, aoQuadrado))
+console.log(somarValoresNumericosETratar(1, 2, dividirPorEleMesmo))
